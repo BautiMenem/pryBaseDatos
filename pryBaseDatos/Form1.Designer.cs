1cf8b5cfcee06dea8fbd04c7fda8d1842fe1f859
root <mgiraudo@ies21.edu.ar>
2023-05-04 14:07:01 -0300

    Agregar archivos de proyecto.

---
+++ pryBaseDatos/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace pryBaseDatos
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.btnLeer = new System.Windows.Forms.Button();
+            this.dtvDatos = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dtvDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(29, 33);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(85, 24);
+            this.btnConectar.TabIndex = 0;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // btnLeer
+            // 
+            this.btnLeer.Location = new System.Drawing.Point(29, 249);
+            this.btnLeer.Name = "btnLeer";
+            this.btnLeer.Size = new System.Drawing.Size(85, 23);
+            this.btnLeer.TabIndex = 1;
+            this.btnLeer.Text = "Leer";
+            this.btnLeer.UseVisualStyleBackColor = true;
+            this.btnLeer.Click += new System.EventHandler(this.btnLeer_Click);
+            // 
+            // dtvDatos
+            // 
+            this.dtvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtvDatos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dtvDatos.Location = new System.Drawing.Point(29, 77);
+            this.dtvDatos.Name = "dtvDatos";
+            this.dtvDatos.Size = new System.Drawing.Size(342, 150);
+            this.dtvDatos.TabIndex = 2;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "xd";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.Name = "Column3";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(402, 308);
+            this.Controls.Add(this.dtvDatos);
+            this.Controls.Add(this.btnLeer);
+            this.Controls.Add(this.btnConectar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.HelpButton = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = ".";
+            ((System.ComponentModel.ISupportInitialize)(this.dtvDatos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.Button btnLeer;
+        private System.Windows.Forms.DataGridView dtvDatos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+    }
+}
+
---
+++ pryBaseDatos/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+
+namespace pryBaseDatos
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        OleDbConnection conn = null;
+        OleDbCommand cmd = null;
+        OleDbDataReader LectorBD = null;
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            
+            conn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=biblio.accdb; ");
+            conn.Open();
+
+            MessageBox.Show("Conectado a base de datos!!!");
+        }
+
+        private void btnLeer_Click(object sender, EventArgs e)
+        {
+            cmd = new OleDbCommand();
+            cmd.Connection = conn;
+            cmd.CommandType = CommandType.TableDirect;
+            cmd.CommandText = "TITULO";
+
+            //MessageBox.Show("Conexion" + " " + cmd.CommandText, "Mesnsaje de conexion");
+
+            LectorBD = cmd.ExecuteReader();
+
+            while (LectorBD.Read())
+            {
+                dtvDatos.Rows.Add(LectorBD[0]);
+                dtvDatos.Rows[].Add(LectorBD[1]);
+                dtvDatos.Rows[].Add(LectorBD[2]);
+            }
+        }
+    }
+}
